7cbf2107013826dd2be7657bb777750b510f70eb
Diego <diegofilipe20115@gmail.com>
2026-05-01 20:59:56 -0300

    feat(users.repository.ts): refactor updateUser function and add type definition for User

--- src/repositories/users.repository.ts
+++ src/repositories/users.repository.ts
@@ -1,43 +1,60 @@
 import { UUID } from "node:crypto";
 import sql from "../config/connection";
 
+type User = {
+  id: UUID;
+  name: string;
+  email: string;
+  password_hash: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
 export async function getAllUsers() {
-    const users = await sql`SELECT * FROM users_maintos ORDER BY created_at DESC`;
-    return users;
+  const users = await sql`SELECT * FROM users_maintos ORDER BY created_at DESC`;
+  return users;
 }
 
 export async function getUserById(id: UUID) {
-    const [user] = await sql`
+  const [user] = await sql`
         SELECT * FROM users_maintos WHERE id = ${id}
     `;
-    if (!user) {
-        throw new Error("User not found");
-    }
-    return user;
+  if (!user) {
+    throw new Error("User not found");
+  }
+  return user;
 }
 
-export async function addUser(user: { name: string, email: string, password_hash: string }) {
-    const [newUser] = await sql`
+export async function addUser(user: {
+  name: string;
+  email: string;
+  password_hash: string;
+}) {
+  const [newUser] = await sql`
         INSERT INTO users_maintos (name, email, password_hash)
         VALUES (${user.name}, ${user.email}, ${user.password_hash})
         RETURNING *
     `;
-    return newUser;
+  return newUser;
 }
 
-export async function updateUser(id: UUID, user: { name?: string, email?: string, password_hash?: string }) {
-    const [updatedUser] = await sql`
+export async function updateUser(
+  id: UUID,
+  user: { name?: string; email?: string; password_hash?: string }
+) {
+  const [updatedUser] = (await sql`
         UPDATE users_maintos
-        SET name = ${user.name || updatedUser.name}, email = ${user.email || updatedUser.email},
-            password_hash = ${user.password_hash || updatedUser.password_hash}
+        SET name = COALESCE(${user.name ?? null}, name), 
+            email = COALESCE(${user.email ?? null}, email),
+            password_hash = COALESCE(${user.password_hash ?? null}, password_hash)
         WHERE id = ${id}
         RETURNING *
-    `;
-    return updatedUser;
+    `) as User[];
+  return updatedUser;
 }
 
 export async function deleteUser(id: UUID) {
-    await sql`
+  await sql`
         DELETE FROM users_maintos WHERE id = ${id}
     `;
 }
